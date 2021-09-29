0f98657cd0468be8db46f530f6247ed73a4b2b81
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-09-29 22:09:20 +0200

    création d'une fonction pour faire le cleaning des commentaires

---
+++ clean_function.py
@@ -0,0 +1,20 @@
+#Libraries
+import re
+import nltk
+from nltk.corpus import stopwords
+#les caractères à enlever 
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;:!?]')
+#Importer la liste des stopwords en français 
+STOPWORDS = nltk.corpus.stopwords.words('french')
+#Liste des mots à ajouter dans la liste des stopwords en français
+words_to_add=['fiche', 'db', 'dpt']
+for word in words_to_add:
+  STOPWORDS.append(word)
+#mots à enlever de la liste des stopwords en français
+STOPWORDS.remove('pas')
+#fonction pour faire le cleaning
+def clean_comment(comment):
+  comment = comment.lower() # lowercase text
+  comment = REPLACE_BY_SPACE_RE.sub(' ', comment) # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space. 
+  comment = ' '.join(word for word in comment.split() if word not in STOPWORDS) # remove stopwors from text
+  return comment
