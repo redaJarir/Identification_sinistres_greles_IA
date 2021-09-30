3158432ccec1af030b2ad5416f4ac228e1935272
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-09-30 21:33:04 +0200

    nouveau modèle entrainé et enregistré

--- data_preparation_function.py
+++ data_preparation_function.py
@@ -1,19 +1,18 @@
 import pickle5 as pickle
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 max_length = 10
 def preparation(df):
-  df
-  BD = []
-  BD=pd.DataFrame(BD)
+  df=pd.read_csv('test1.csv', sep=';', encoding = "ISO-8859-1")
   df.loc[df['LI_DSC_SIGMA'].isnull(), 'LI_DSC_SIGMA']=df['LI_DSC_ADS']
   df.rename(columns={'LI_DSC_SIGMA': 'Comment'}, inplace=True)
-  BD['Comment'] = df['Comment'].apply(clean_comment)
-  BD['Comment'] = BD['Comment'].str.replace('\d+', '')
-  for i in range (0,len(BD)):
-    BD.loc[i, 'Comment']=stem_comment(BD.loc[i, 'Comment'])
+  df['Comment'] = df['Comment'].apply(clean_comment)
+  df['Comment'] = df['Comment'].str.replace('\d+', '')
+  for i in range (len(df)):
+    df.loc[i, 'Comment']=stem_comment(df.loc[i, 'Comment'])
   with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
     X = tokenizer.texts_to_sequences(df["Comment"].to_numpy())
     X = pad_sequences(X, maxlen=max_length,truncating="post", padding="post")
-    return(X)
+  return(X)
