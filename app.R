c7f81739d5991dcc452031c6f0d979b8bd11cfb6
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-03 17:00:39 +0200

    button config and sourcing python scripts (premier appel au modèle lstm et à la fonction Recherche V)

--- app.R
+++ app.R
@@ -1,11 +1,17 @@
 library(shiny)
-library(fresh)
-library(tensorflow)
-library(keras)
 library(reticulate)
+library(fresh)
+library(openxlsx)
+#library(tensorflow)
+#library(keras)
+library(vroom)
+source_python("clean_function.py")
+source_python("stem_function.py")
 source_python("data_preparation_function.py")
-
-ui <- navbarPage("Identification des sinistres causés par la grêle",
+source_python("predict_function.py")
+source_python("Recherche_V_function.py")
+source_python("classifier_function.py")
+ui <- navbarPage(strong("Identification des sinistres causés par la grêle"),
                  header=use_theme(create_theme(
                    theme = "default",
                    bs_vars_navbar(
@@ -15,19 +21,59 @@ ui <- navbarPage("Identification des sinistres causés par la grêle",
                    ),
                    output_file = NULL
                  )),
-                 column(6, fileInput("file","Sélectionner le fichier des données"),
-                        actionButton("classifier", "Classifier les sinistres", style="color: #fff; background-color: green; border-color: black"),
-                        actionButton("Telecharger", "Télécharger les résultats", style="color: #fff; background-color: green; border-color: black")))
-
+                 tags$head(
+                   tags$style(HTML("
+                    .shiny-output-error-validation {
+                    color: red;
+                      }
+                  "))
+                 ),
+                 img(src='groupama.jpg', height="10%", width="10%"),
+                 br(),
+                 br(),
+                 tags$style(".shiny-file-input-progress {display: none}"),
+                 sidebarLayout(
+                   sidebarPanel(fileInput("myfile","Sélectionner le fichier des données", accept = ".csv"),
+                  actionButton("classifier", "Classifier les sinistres", style="color: #fff; background-color: green; border-color: black"),
+                                br(),
+                                br(),
+                                downloadButton("Telecharger", "Télécharger les résultats", style="color: #fff; background-color: green; border-color: black")
+                     ),
+                   mainPanel(tableOutput("mytable"))
+                 )
+                )
+                 
 server <- function(input, output, session) {
   
-  dataset<-reactive({ 
-    inFile <- input$file
-    dat<-read_xlsx(inFile$datapath, sheet =  1)
-    return(dat)
-  })
-  observeEvent({input$classifier, })
+
+    myfile<-reactive(input$myfile)
+    data <- reactive({
+    req(myfile())
+    
+    # as shown in the book, lets make sure the uploaded file is a csv
+    ext <- tools::file_ext(myfile())
+    
+    validate(need(ext == "csv", "Erreur : Télécharger un fichier .csv s'il vous plaît"))
+    dataset <- vroom::vroom(myfile()$datapath, col_names=TRUE, show_col_types = FALSE, locale = vroom::locale(encoding = "ISO-8859-1"))
+    return(dataset)
+    })
   
-}
+    pred_verif <- eventReactive(input$classifier,{
+    data_class<-classifier(data())
+    return(data_class)
+    })
+  
+    output$mytable <- renderTable({
+      my_table <- pred_verif()
+      sinistres_verif<-my_table[my_table[,'Class']=='sinistre à vérifier',]
+      if(is.null(sinistres_verif)){
+        return(NULL)
+      }else{
+        sinistres_verif
+    }})
+    output$Telecharger<-downloadHandler(filename='sinistres classifiés.csv',
+    content=function(file){read.csv(pred_verif(), file, col.names=TRUE, append=FALSE)})
+  
+  }
 
 shinyApp(ui, server)
