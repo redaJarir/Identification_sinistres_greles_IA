ee18401aa3f7f990847cb08a64557a7a353c85d6
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-06 22:54:17 +0200

    Ajout d'une troisième variable à concaténer avec les deux autres pour avoir les commentaires finaux

--- Recherche_V_function.py
+++ Recherche_V_function.py
@@ -1,15 +1,16 @@
 
 def Recherche_V(df):
-  BD=pd.DataFrame(df)
-  BD.loc[df['LI_DSC_SIGMA'].isnull(), 'LI_DSC_SIGMA']=BD['LI_DSC_ADS']
-  BD.rename(columns={'LI_DSC_SIGMA': 'Comment'}, inplace=True)
-  BD['Comment'] = BD['Comment'].apply(clean_comment)
-  BD['Comment'] = BD['Comment'].str.replace('\d+', '')
-  for i in range (len(BD)):
-    BD.loc[i, 'Comment']=stem_comment(BD.loc[i, 'Comment'])
-    a='grel' in BD.loc[i, 'Comment']
-    if a==0:
-        df.loc[i, 'Recherche_V_class']='non grele'
-    else:
+  df['LI_DSC_ADS'] = df['LI_DSC_ADS'].astype(str)
+  df['LI_DSC_SIGMA'] = df['LI_DSC_SIGMA'].astype(str)
+  df['LI_CAUSE'] = df['LI_CAUSE'].astype(str)
+  df['Comment'] = df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS + ' '+df.LI_CAUSE)
+  df['Comment'] = df['Comment'].apply(clean_comment)
+  df['Comment'] = df['Comment'].str.replace('\d+', '')
+  matches=['grel', 'grêl', 'gr^l']
+  for i in range (len(df)):
+    df.loc[i, 'Comment']=stem_comment(df.loc[i, 'Comment'])
+    if any(a in df.loc[i, 'Comment'] for a in matches):
         df.loc[i, 'Recherche_V_class']='grele'
+    else:
+        df.loc[i, 'Recherche_V_class']='non grele'
   return(df)
