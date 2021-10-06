2b07fb4bb7f668d086dbe03ed09e64e69ddec061
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-06 22:56:33 +0200

    ajout d'une troisième variable

--- data_preparation_function.py
+++ data_preparation_function.py
@@ -1,12 +1,14 @@
 import pickle5 as pickle
+
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 max_length = 10
 def preparation(df):
-  df=pd.read_csv('test1.csv', sep=';', encoding = "ISO-8859-1")
-  df.loc[df['LI_DSC_SIGMA'].isnull(), 'LI_DSC_SIGMA']=df['LI_DSC_ADS']
-  df.rename(columns={'LI_DSC_SIGMA': 'Comment'}, inplace=True)
+  df['LI_DSC_ADS'] = df['LI_DSC_ADS'].astype(str)
+  df['LI_DSC_SIGMA'] = df['LI_DSC_SIGMA'].astype(str)
+  df['LI_CAUSE'] = df['LI_CAUSE'].astype(str)
+  df['Comment'] = df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS+' '+df.LI_CAUSE)
   df['Comment'] = df['Comment'].apply(clean_comment)
   df['Comment'] = df['Comment'].str.replace('\d+', '')
   for i in range (len(df)):
