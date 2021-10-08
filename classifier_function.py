054fe71858c248b45f28bbcd5fda36497da7f7c1
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-08 17:23:06 +0200

    ajout d'un commentaire et cacher les colonnes de la recherche v et du modèle lstm

--- classifier_function.py
+++ classifier_function.py
@@ -1,8 +1,8 @@
-
+#fonction de la solution hybride
 def classifier(df):
   df_Recherche_V = Recherche_V(df)
   df_lstm = predict(df)
-  for i in range (len(df_Recherche_V)):
+  for i in range (len(df)):
     if df_Recherche_V.loc[i, 'Recherche_V_class']=='grele' and df_lstm.loc[i, 'lstm_class']=='grele':
       df.loc[i, 'Class']='grele'
     elif df_Recherche_V.loc[i, 'Recherche_V_class']=='non grele' and df_lstm.loc[i, 'lstm_class']=='grele':
@@ -11,4 +11,5 @@ def classifier(df):
       df.loc[i, 'Class']='non grele'
     elif df_Recherche_V.loc[i, 'Recherche_V_class']=='grele' and df_lstm.loc[i, 'lstm_class']=='non grele':
       df.loc[i, 'Class']='sinistre à vérifier'
-  return(df)
+  return(df.loc[:, (df.columns != 'Recherche_V_class')&(df.columns != 'lstm_class')&(df.columns != 'Comment')])
+
