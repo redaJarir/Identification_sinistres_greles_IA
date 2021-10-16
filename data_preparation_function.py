1655724752dc0063b198e54bf66d6892e0c2819b
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-16 11:07:32 +0200

    enlever la dernière colonne sur laquelle le modèle lstm n'a pas été entrainé

--- data_preparation_function.py
+++ data_preparation_function.py
@@ -8,14 +8,15 @@ max_length = 10
 def preparation(df):
   df['LI_DSC_ADS'] = df['LI_DSC_ADS'].astype(str)
   df['LI_DSC_SIGMA'] = df['LI_DSC_SIGMA'].astype(str)
-  df['LI_CAUSE'] = df['LI_CAUSE'].astype(str)
-  df['Comment'] = df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS+' '+df.LI_CAUSE)
-  df['Comment'] = df['Comment'].apply(clean_comment)
-  df['Comment'] = df['Comment'].str.replace('\d+', '')
+  df['Comment_lstm'] = df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS)
+  df['Comment_lstm'] = df['Comment_lstm'].apply(clean_comment)
+  df['Comment_lstm'] = df['Comment_lstm'].str.replace('\d+', '')
   for i in range (len(df)):
-    df.loc[i, 'Comment']=stem_comment(df.loc[i, 'Comment'])
+    df.loc[i, 'Comment_lstm']=stem_comment(df.loc[i, 'Comment_lstm'])
   with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
-    X = tokenizer.texts_to_sequences(df["Comment"].to_numpy())
+    X = tokenizer.texts_to_sequences(df["Comment_lstm"].to_numpy())
     X = pad_sequences(X, maxlen=max_length,truncating="post", padding="post")
   return(X)
+
+
