d15e16343e55125d30123cbd41bcfe24646df36d
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-16 11:06:57 +0200

    modification d'une condition qui était inutile

--- classifier_function.py
+++ classifier_function.py
@@ -3,13 +3,11 @@ def classifier(df):
   df_Recherche_V = Recherche_V(df)
   df_lstm = predict(df)
   for i in range (len(df)):
-    if df_Recherche_V.loc[i, 'Recherche_V_class']=='grele' and df_lstm.loc[i, 'lstm_class']=='grele':
+    if df_Recherche_V.loc[i, 'Recherche_V_class']=='non grele':
+      df.loc[i, 'Class']='non grele'
+    elif df_Recherche_V.loc[i, 'Recherche_V_class']=='grele' and df_lstm.loc[i, 'lstm_class']=='grele':
       df.loc[i, 'Class']='grele'
-    elif df_Recherche_V.loc[i, 'Recherche_V_class']=='non grele' and df_lstm.loc[i, 'lstm_class']=='grele':
-      df.loc[i, 'Class']='non grele'
-    elif df_Recherche_V.loc[i, 'Recherche_V_class']=='non grele' and df_lstm.loc[i, 'lstm_class']=='non grele': 
-      df.loc[i, 'Class']='non grele'
     elif df_Recherche_V.loc[i, 'Recherche_V_class']=='grele' and df_lstm.loc[i, 'lstm_class']=='non grele':
       df.loc[i, 'Class']='sinistre à vérifier'
-  return(df.loc[:, (df.columns != 'Recherche_V_class')&(df.columns != 'lstm_class')&(df.columns != 'Comment')])
+  return(df.loc[:, (df.columns != 'Recherche_V_class')&(df.columns != 'lstm_class')&(df.columns != 'Comment_RV')&(df.columns != 'Comment_lstm')])
 
