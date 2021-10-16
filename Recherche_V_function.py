ee8749d5837f2d10a44363863517422abfbc6016
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-16 11:06:25 +0200

    renommer quelques variables

--- Recherche_V_function.py
+++ Recherche_V_function.py
@@ -3,14 +3,15 @@ def Recherche_V(df):
   df['LI_DSC_ADS'] = df['LI_DSC_ADS'].astype(str)
   df['LI_DSC_SIGMA'] = df['LI_DSC_SIGMA'].astype(str)
   df['LI_CAUSE'] = df['LI_CAUSE'].astype(str)
-  df['Comment'] = df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS + ' '+df.LI_CAUSE)
-  df['Comment'] = df['Comment'].apply(clean_comment)
-  df['Comment'] = df['Comment'].str.replace('\d+', '')
+  df['Comment_RV'] = df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS + ' '+df.LI_CAUSE)
+  df['Comment_RV'] = df['Comment_RV'].apply(clean_comment)
+  df['Comment_RV'] = df['Comment_RV'].str.replace('\d+', '')
   matches=['grel', 'grêl', 'gr^l']
   for i in range (len(df)):
-    df.loc[i, 'Comment']=stem_comment(df.loc[i, 'Comment'])
-    if any(a in df.loc[i, 'Comment'] for a in matches):
+    df.loc[i, 'Comment_RV']=stem_comment(df.loc[i, 'Comment_RV'])
+    if any(a in df.loc[i, 'Comment_RV'] for a in matches):
         df.loc[i, 'Recherche_V_class']='grele'
     else:
         df.loc[i, 'Recherche_V_class']='non grele'
   return(df)
+
