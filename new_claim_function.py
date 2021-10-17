ed4e5c6181971831539e8a013f7ff3e1d9bcfbad
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-17 12:14:01 +0200

    deleted by mistake

--- new_claim_function.py
+++
@@ -1,4 +0,0 @@
-#fonction pour identifier les nouveaux sinistres
-def nv_sinistres(df_n,df_n_1):
-  table_nouveaux_sinistres = df_n.merge(df_n_1['ID_ADS'], how = 'left' ,indicator=True).loc[lambda x : x['_merge']=='left_only'].drop('_merge', 1).reset_index(drop=True)
-  return(table_nouveaux_sinistres)
