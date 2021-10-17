2c4b46a88cce3458bcb64bcd949f2a693e8c1243
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-17 12:12:23 +0200

    la dernière version de l'application (après avoir ajouté le fichier pickle)

--- app.R
+++ app.R
@@ -9,12 +9,12 @@ library(DT)
 library(dplyr)
 source_python("clean_function.py")
 source_python("stem_function.py")
-source_python("data_preparation_function.py")
-source_python("predict_function.py")
+source_python("data_preparation_for_lstm_function.py")
+source_python("lstm_predictions_function.py")
 source_python("Recherche_V_function.py")
-source_python("classifier_function.py")
-source_python("new_claim_function.py")
+source_python("initial_classification_function.py")
 source_python("final_classification_function.py")
+source_python("classes_new_claims_function.py")
 ui <- dashboardPage(skin='green',
   dashboardHeader(
     title = "Identification des sinistres grêles",
@@ -24,10 +24,7 @@ ui <- dashboardPage(skin='green',
                                 style = "padding-top:10px; padding-bottom:10px;"),
                               class = "dropdown")
   ),
-  dashboardSidebar(sidebarMenu(
-    menuItem(strong("Identification générale"), tabName = "general", icon = icon("home")),
-    menuItem(strong("Identification périodique"), tabName = "period", icon = icon("dashboard"))
-  )),
+  dashboardSidebar(),
   dashboardBody(
     tags$head(tags$style(HTML('
       .main-header .logo {
@@ -39,8 +36,6 @@ ui <- dashboardPage(skin='green',
     tags$head(
       tags$style(HTML(".main-sidebar {background-color:  #1E8449 !important;}"))
     ),
-    tabItems(
-      tabItem(tabName = 'general',
               br(),
               tags$head(
                 tags$style(HTML("
@@ -51,162 +46,83 @@ ui <- dashboardPage(skin='green',
               tags$style(HTML("
     .tabbable > .nav > li > a {background-color: #7DCEA0;  color:black; width: 400PX;}
   ")),
-  column(fileInput("general_file","Sélectionner le fichier Excel des données"),br(),
+  column(fileInput("my_file","Sélectionner le fichier Excel des données"),br(),
                               
-                  actionButton("classifier_general",
+                  actionButton("classifier",
                     strong("Classifier les sinistres"),
                     style="color: #fff; background-color: green; border-color: black; width:200px"),
                   br(),
                   br(),
-                  actionButton("classifier_grele_general",
+                  actionButton("classifier_grele",
                     strong("Classifier grêle"),
                     style="color: #fff; background-color: green; border-color: black; width:200px"),
                   br(),
                   br(),
-                  downloadButton("Telecharger_general",
+                  downloadButton("Telecharger",
                     strong("Télécharger les résultats"),
                     style="color: #fff; background-color: green; border-color: black; width:200px"),
                     width = 4),
                        
-  column(dataTableOutput("Table_sinistres_a_verifier_general"), width=8)),
+  column(dataTableOutput("Table_sinistres_a_verifier"), width=8)
               
-      tabItem(tabName = 'period',
-              br(),
-              tags$head(tags$style(HTML(".shiny-output-error-validation {color: red;}"))
-                 ),
-              tags$style(".shiny-file-input-progress {display: none}"),
-              tags$style(HTML(".tabbable > .nav > li > a {background-color: #7DCEA0;  color:black; width: 400PX;}")
-                         ),
-              column(fileInput("file_n","Sélectionner le fichier Excel des données de l'année n"),
-                     br(),
-                    fileInput("file_n_1","Sélectionner le fichier Excel des données de l'année n-1"),
-                    br(),
-                    actionButton("classifier_period", strong("Classifier les sinistres"),
-                        style="color: #fff; background-color: green; border-color: black"),
-                    br(),
-                    br(),
-                    actionButton("classifier_grele_period",strong("Classifier grêle"),
-                        style="color: #fff; background-color: green; border-color: black"),
-                    br(),
-                    br(),
-                    downloadButton("Telecharger_period", strong("Télécharger les résultats"),
-                        style="color: #fff; background-color: green; border-color: black"),width = 4),
-                 
-              column(dataTableOutput("Table_sinistres_a_verifier_period"), width=8)))))
+      
+              ))
                 
                    
                  
                  
 server <- function(input, output, session) {
   
-    general_file<-reactive(input$general_file)
-    file_n<-reactive(input$file_n)
-    file_n_1<-reactive(input$file_n_1)
+    my_file<-reactive(input$my_file)
+  
     
-    general_data<- reactive({
-      req(general_file())
-      ext <- tools::file_ext(general_file())
+    my_data<- reactive({
+      req(my_file())
+      ext <- tools::file_ext(my_file())
       validate(need(ext == "xlsx", "Erreur : Télécharger un fichier .xlsx s'il vous plaît"))
-      file.rename(general_file()$datapath,paste(general_file()$datapath, ".xlsx", sep=""))
-      general_dataset<-read.xlsx(paste(general_file()$datapath, ".xlsx", sep=""), 1)
-      return(general_dataset)
+      file.rename(my_file()$datapath,paste(my_file()$datapath, ".xlsx", sep=""))
+      my_dataset<-read.xlsx(paste(my_file()$datapath, ".xlsx", sep=""), 1)
+      return(my_dataset)
     })
     
-    classes_sinistres_general <- eventReactive(input$classifier_general,{
-      data_class_general<-classifier(general_data())
-      return(data_class_general)
+    classes_claims<- eventReactive(input$classifier,{
+      data_class<-new_claims_class(my_data())
+      return(data_class)
     })
-    sinistres_a_verifier_general<-reactive(classes_sinistres_general()[classes_sinistres_general()[,'Class']=='sinistre à vérifier',])
+    claims_to_check<-reactive({
+      classes_claims()[classes_claims()[,'Class']=='sinistre à vérifier',]})
     
-    output$Table_sinistres_a_verifier_general<-renderDataTable({
-      if(is.null(sinistres_a_verifier_general())){
-        return(NULL)
-      }else{
-        sinistres_a_verifier_general()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')]
-      }
-    }, options = list(scrollX = TRUE, reactable(
-      data = sinistres_a_verifier_general()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')],
+    output$Table_sinistres_a_verifier<-renderDataTable({
+      claims_to_check()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')]
+      }, options = list(scrollX = TRUE, reactable(
+      data = claims_to_check()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')],
       selection = "multiple",
-      selectionId = "tableid1",
       onClick = "select",
       defaultSelected = NULL,
       fullWidth = FALSE
     )))
     
-    sinistres_a_verifier_grele_general <- eventReactive(input$classifier_grele_general,{
-      selected_rows=input$Table_sinistres_a_verifier_general_rows_selected
-      
-      sinistres_greles<-sinistres_a_verifier_general()[selected_rows,]
-      sinistres_greles[,'Class']<-'grele'
-      return(sinistres_greles)
+    claims_to_check_hail<- eventReactive(input$classifier_grele,{
+        selected_rows=input$Table_sinistres_a_verifier_rows_selected
+        sinistres_greles<-claims_to_check()[selected_rows,]
+        sinistres_greles[,'Class']<-'grele'
+        sinistres_greles
     })
     
     
     
-    output$Telecharger_general<-downloadHandler(filename=function(){'classes_sinistres.xlsx'},
+    output$Telecharger<-downloadHandler(filename=function(){'classes_sinistres.xlsx'},
                                                content=function(file){
-                                                 table_classes_sinistres<-table_finale(classes_sinistres_general(),sinistres_a_verifier_general(),sinistres_a_verifier_grele_general())
+                                                 if(length(input$Table_sinistres_a_verifier_rows_selected) > 0){
+                                                   sinistres_greles=claims_to_check_hail()}
+                                                 else{
+                                                   sinistres_greles<-data.frame()
+                                                 }
+                                                 table_classes_sinistres<-table_finale(classes_claims(),claims_to_check(),sinistres_greles)
                                                  write.xlsx(table_classes_sinistres, file, sheetName = "Sinistres classifiés")
+                                                 
                                                })
     
-    data_n <- reactive({
-    req(file_n())
-    ext <- tools::file_ext(file_n())
-    validate(need(ext == "xlsx", "Erreur : Télécharger un fichier .xlsx s'il vous plaît"))
-    file.rename(file_n()$datapath,paste(file_n()$datapath, ".xlsx", sep=""))
-    dataset_n<-read.xlsx(paste(file_n()$datapath, ".xlsx", sep=""), 1)
-    return(dataset_n)
-    })
-    
-    data_n_1 <- reactive({
-      req(file_n_1())
-      ext <- tools::file_ext(file_n_1())
-      validate(need(ext == "xlsx", "Erreur : Télécharger un fichier .xlsx s'il vous plaît"))
-      file.rename(file_n_1()$datapath,paste(file_n_1()$datapath, ".xlsx", sep=""))
-      dataset_n_1<-read.xlsx(paste(file_n_1()$datapath, ".xlsx", sep=""), 1)
-      return(dataset_n_1)
-    })
-    
-    
-    classes_nouveaux_sinistres_period <- eventReactive(input$classifier_period,{
-    nouveaux_sinistres<-nv_sinistres(data_n(), data_n_1())
-    data_class_period<-classifier(nouveaux_sinistres)
-    return(data_class_period)
-    })
-    
-    sinistres_a_verifier_period<-reactive(classes_nouveaux_sinistres_period()[classes_nouveaux_sinistres_period()[,'Class']=='sinistre à vérifier',])
-    
-    
-    output$Table_sinistres_a_verifier_period<-renderDataTable({
-      if(is.null(sinistres_a_verifier_period())){
-        return(NULL)
-      }else{
-        sinistres_a_verifier_period()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')]
-    }
-    }, options = list(scrollX = TRUE, reactable(
-      data = sinistres_a_verifier_period()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')],
-      selection = "multiple",
-      selectionId = "tableid1",
-      onClick = "select",
-      defaultSelected = NULL,
-      fullWidth = FALSE
-    )))
-    
-    sinistres_a_verifier_grele_period <- eventReactive(input$classifier_grele_period,{
-      selected_rows=input$Table_sinistres_a_verifier_period_rows_selected
-      
-      sinistres_greles<-sinistres_a_verifier_period()[selected_rows,]
-      sinistres_greles[,'Class']<-'grele'
-      return(sinistres_greles)
-    })
-    
-    output$Telecharger_period<-downloadHandler(filename=function(){'classes_nouveaux_sinistres.xlsx'},
-                                                content=function(file){
-                                                  table_classes_sinistres<-table_finale(classes_nouveaux_sinistres_period(),sinistres_a_verifier_period(),sinistres_a_verifier_grele_period())
-                                                  write.xlsx(table_classes_sinistres, file, sheetName = "Nouveaux sinistres classifiés")
-                                                })
-    
-  
   }
 
 shinyApp(ui, server)
