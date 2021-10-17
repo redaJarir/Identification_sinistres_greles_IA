146a23ad11c3445ed604ab8dbf3772043df8d71b
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-17 12:07:53 +0200

    fonction pour détecter les nouveaux sinistres en comparant les ID des sinistres reçus avec les ID des sinistres dans la mémoire et actualiser les fichier pickle des sinistres déjà classifiés

---
+++ classes_new_claims_function.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import pickle5 as pickle
+import os
+def new_claims_class(df):
+  if os.path.isfile("ID_classified.pickle"):
+    with open('ID_classified.pickle', 'rb') as f:
+        ID_classified = pickle.load(f)
+        saved_ID_df=pd.DataFrame(ID_classified, columns=['ID_ADS'])
+        table_nouveaux_sinistres = df.merge(saved_ID_df['ID_ADS'], how = 'left' ,indicator=True).loc[lambda x : x['_merge']=='left_only'].drop('_merge', 1).reset_index(drop=True)
+        class_new_claims=classifier(table_nouveaux_sinistres)
+        ID_classified=class_new_claims['ID_ADS'].to_list()
+        save_ID=saved_ID_df['ID_ADS'].to_list()+ID_classified
+    with open('ID_classified.pickle', 'wb') as f:
+        pickle.dump(save_ID, f)
+  else:
+    with open('ID_classified.pickle', 'wb') as f:
+      class_new_claims=classifier(df)
+      ID_classified=class_new_claims['ID_ADS'].to_list()
+      pickle.dump(ID_classified, f)
+  return(class_new_claims)
+
