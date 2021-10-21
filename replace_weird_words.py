92cb964a8f8b9c569dc05fa197f3ede40b9fd804
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-10-21 12:20:54 +0200

    Ajout de quelques écritures bizarres du mot grele

--- replace_weird_words.py
+++ replace_weird_words.py
@@ -1,6 +1,6 @@
 
 def replace_weird_words(comment):
-  ecritures_bizarres=['gr\x88le', 'grele', 'gêle', 'gr le','grële', 'grle', 'gr^le']
+  ecritures_bizarres=['gr\x88le','grele','gêle','gr le','græle','grële','g^le','gr}le','grle','gr^le']
   comment=comment.lower()
   for a in ecritures_bizarres:
     if a in comment:
