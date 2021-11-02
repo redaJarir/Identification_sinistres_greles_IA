d7229180ce205a8e30fad1aab34984c73b61dd7d
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-11-02 15:23:55 +0100

    suppression de la fonction replace_weird_words

--- Recherche_V_function.py
+++ Recherche_V_function.py
@@ -3,10 +3,7 @@ def Recherche_V(df):
   df['LI_DSC_ADS'] = df['LI_DSC_ADS'].astype(str)
   df['LI_DSC_SIGMA'] = df['LI_DSC_SIGMA'].astype(str)
   df['LI_CAUSE'] = df['LI_CAUSE'].astype(str)
-  df['LI_DSC_ADS_1']=df['LI_DSC_ADS'].apply(replace_weird_words)
-  df['LI_DSC_SIGMA_1']=df['LI_DSC_SIGMA'].apply(replace_weird_words)
-  df['LI_CAUSE_1']=df['LI_CAUSE'].apply(replace_weird_words)
-  df['Comment_RV'] = df.LI_DSC_SIGMA_1.str.cat(' '+df.LI_DSC_ADS_1 + ' '+df.LI_CAUSE_1)
+  df['Comment_RV'] = df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS + ' '+df.LI_CAUSE)
   df['Comment_RV'] = df['Comment_RV'].apply(clean_comment)
   df['Comment_RV'] = df['Comment_RV'].str.replace('\d+', '')
   matches=['grel', 'grêl']
