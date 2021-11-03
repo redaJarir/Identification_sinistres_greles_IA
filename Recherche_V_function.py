a2a7c966a6c0f4737bb31c17e31f219d13013096
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-11-03 10:53:50 +0100

    ajout du mot à chercher dans les commentaires

--- Recherche_V_function.py
+++ Recherche_V_function.py
@@ -6,7 +6,7 @@ def Recherche_V(df):
   df['Comment_RV'] = df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS + ' '+df.LI_CAUSE)
   df['Comment_RV'] = df['Comment_RV'].apply(clean_comment)
   df['Comment_RV'] = df['Comment_RV'].str.replace('\d+', '')
-  matches=['grel', 'grêl']
+  matches=['grel']
   for i in range (len(df)):
     df.loc[i, 'Comment_RV']=stem_comment(df.loc[i, 'Comment_RV'])
     if any(a in df.loc[i, 'Comment_RV'] for a in matches):
