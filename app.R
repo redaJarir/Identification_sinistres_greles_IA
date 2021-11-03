bbbb787c64039a7ae5934b7c143946509c504d58
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-11-03 14:42:18 +0100

    la nouvelle version de l'application après avoir fait quelques modifications (indentations) pour faciliter la lecture du code

--- app.R
+++ app.R
@@ -4,7 +4,6 @@ library(reactable)
 library(fresh)
 library(shinydashboard)
 library(DT)
-library(xlsx)
 source_python("clean_function.py")
 source_python("stem_function.py")
 source_python("data_preparation_for_lstm_function.py")
@@ -14,103 +13,165 @@ source_python("initial_classification_function.py")
 source_python("final_classification_function.py")
 source_python("classes_new_claims_function.py")
 ui <- dashboardPage(skin='green',
-  dashboardHeader(
-    title = "Identification des sinistres grêles",
-    titleWidth = 400, tags$li(a(href = 'http://www.groupama.fr',
-                                img(src = 'groupama.jpg',
-                                    title = "Groupama", height = "30px"),
-                                style = "padding-top:10px; padding-bottom:10px;"),
-                              class = "dropdown")),
-  dashboardSidebar(),
-  dashboardBody(
-    tags$head(tags$style(HTML('
-      .main-header .logo {
-        font-family: "Georgia", Times, "Times New Roman", serif;
-        font-weight: bold;
-        font-size: 20px;}'))),
-    tags$head(
-      tags$style(HTML("
-        .main-sidebar {background-color:  #1E8449 !important;}"))),
-    br(),
-    tags$head(tags$style(HTML("
-        .shiny-output-error-validation {
-          color: red;
-          }"))),
-    tags$style(".shiny-file-input-progress {display: none}"),
-    tags$style(HTML("
-    .tabbable > .nav > li > a {background-color: #7DCEA0;
-    color:black; width: 400PX;}
-      ")),
-    column(fileInput("my_file",
-            "Importer un fichier Excel de données"),
-          br(),
-          actionButton("classifier",
-            strong("Classifier les sinistres"),
-            style="color: #fff; background-color: green; 
-            border-color: black;border-width: 2px; width:200px"),
-          br(),
-          br(),
-          actionButton("classifier_grele",
-            strong("Classifier grêle"),
-            style="color: #fff; background-color: green; 
-            border-color: black;border-width: 2px; width:200px"),
-          br(),
-          br(),
-          downloadButton("Telecharger",
-            strong("Télécharger les résultats"),
-            style="color: #fff; background-color: green; 
-            border-color: black;border-width: 2px; width:200px"),
-                    width = 4),
-                       
-      column(dataTableOutput("Table_sinistres_a_verifier"), width=8)
-              
-      
-              ))
-                
-                   
+                    dashboardHeader(
+                      title = "Identification des sinistres grêles",
+                      titleWidth = 400, 
+                      tags$li(
+                        a(
+                          href = 'http://www.groupama.fr',
+                          img(
+                            src = 'groupama.jpg',
+                            title = "Groupama", 
+                            height = "30px"
+                          ),
+                          style = "padding-top:10px; padding-bottom:10px;"
+                        ),
+                        class = "dropdown"
+                      )
+                    ),
+                    dashboardSidebar(),
+                    dashboardBody(
+                      tags$head(
+                        tags$style(
+                          HTML('
+            .main-header .logo {
+            font-family: "Georgia", Times, "Times New Roman", serif;
+            font-weight: bold;
+            font-size: 20px;}'
+                          )
+                        )
+                      ),
+                      tags$head(
+                        tags$style(
+                          HTML("
+            .main-sidebar {background-color:  #1E8449 !important;}"
+                          )
+                        )
+                      ),
+                      br(),
+                      tags$head(
+                        tags$style(
+                          HTML(
+                            ".shiny-output-error-validation {color: red;}"
+                          )
+                        )
+                      ),
+                      tags$style(".shiny-file-input-progress {display: none}"),
+                      tags$style(
+                        HTML(
+                          ".tabbable > .nav > li > a {background-color: #7DCEA0;color:black; width: 400PX;}"
+                        )
+                      ),
+                      column(
+                        fileInput("my_file","Importer un fichier Excel de données"),
+                        br(),
+                        actionButton(
+                          "classifier",
+                          strong("Classifier les sinistres"),
+                          style="color: #fff; 
+              background-color: green; 
+              border-color: black;
+              border-width: 2px; 
+              width:200px"
+                        ),
+                        br(),
+                        br(),
+                        actionButton(
+                          "classifier_grele",
+                          strong("Classifier grêle"),
+                          style="color: #fff; 
+              background-color: green; 
+              border-color: black;
+              border-width: 2px; 
+              width:200px"
+                        ),
+                        br(),
+                        br(),
+                        downloadButton(
+                          "Telecharger",
+                          strong("Télécharger les résultats"),
+                          style="color: #fff; 
+              background-color: green; 
+              border-color: black;
+              border-width: 2px; 
+              width:200px"
+                        ),
+                        width = 4
+                      ),
+                      
+                      column(
+                        dataTableOutput("Table_sinistres_a_verifier"), 
+                        width=8
+                      )
+                      
+                    )
+)
+
+
 server <- function(input, output, session) {
   
-    my_file<-reactive(input$my_file)
-
-    classes_claims<- eventReactive(input$classifier,{
+  my_file<-reactive(input$my_file)
+  
+  classes_claims<- eventReactive(
+    input$classifier,
+    {
       req(my_file())
       ext <- tools::file_ext(my_file()$datapath)
-      validate(need(ext == "xlsx", 
-        "Erreur : Télécharger un fichier .xlsx s'il vous plaît"))
+      validate(
+        need(
+          ext == "xlsx", 
+          "Erreur : Télécharger un fichier .xlsx s'il vous plaît"
+        )
+      )
       data_class<-new_claims_class(my_file()$datapath)
       return(data_class)
-    })
-    claims_to_check<-reactive({
-      classes_claims()[classes_claims()[,'Class']=='sinistre à vérifier',]})
-    
-    output$Table_sinistres_a_verifier<-renderDataTable({
-      claims_to_check()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')]
-      }, options = list(scrollX = TRUE, reactable(
-      data = claims_to_check()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')],
-      selection = "multiple",
-      onClick = "select",
-      defaultSelected = NULL,
-      fullWidth = FALSE
-    )))
-    
-    claims_to_check_hail<- eventReactive(input$classifier_grele,{
-        selected_rows=input$Table_sinistres_a_verifier_rows_selected
-        sinistres_greles<-claims_to_check()[selected_rows,]
-        sinistres_greles[,'Class']<-'grele'
-        sinistres_greles
-    })
-    
-    output$Telecharger<-downloadHandler(filename=function(){'classes_sinistres.xlsx'},
-                          content=function(file){
-                          if(length(input$Table_sinistres_a_verifier_rows_selected) > 0){
-                                sinistres_greles=claims_to_check_hail()
-                                }
-                          else{
-                                sinistres_greles<-data.frame()
-                              }
-                          table_classes_sinistres<-table_finale(classes_claims(),claims_to_check(),sinistres_greles)
-                          write.xlsx(table_classes_sinistres, file, sheetName = "Sinistres classifiés")
-                          })
     }
+  )
+  
+  claims_to_check<-reactive(
+    {
+      classes_claims()[classes_claims()[,'Class']=='sinistre à vérifier',]
+    }
+  )
+  
+  output$Table_sinistres_a_verifier<-renderDataTable(
+    {
+      claims_to_check()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')]
+    }, 
+    options = list(
+      scrollX = TRUE, 
+      reactable(
+        data = claims_to_check()[,c('ID_ADS', 'LI_DSC_SIGMA', 'LI_DSC_ADS', 'LI_CAUSE', 'Class')],
+        selection = "multiple",
+        onClick = "select",
+        defaultSelected = NULL,
+        fullWidth = FALSE
+      )
+    )
+  )
+  
+  claims_to_check_hail<- eventReactive(
+    input$classifier_grele,
+    {
+      selected_rows=input$Table_sinistres_a_verifier_rows_selected
+      sinistres_greles<-claims_to_check()[selected_rows,]
+      sinistres_greles[,'Class']<-'grele'
+      sinistres_greles
+    }
+  )
+  
+  output$Telecharger<-downloadHandler(
+    filename=function(){'classes_sinistres.xlsx'},
+    content=function(file){
+      if(length(input$Table_sinistres_a_verifier_rows_selected) > 0){
+        sinistres_greles=claims_to_check_hail()
+      }else{
+        sinistres_greles<-data.frame()
+      }
+      table_classes_sinistres<-table_finale(classes_claims(),claims_to_check(),sinistres_greles)
+      write.xlsx(table_classes_sinistres, file, sheetName = "Sinistres classifiés")
+    }
+  )
+}
 
 shinyApp(ui, server)
