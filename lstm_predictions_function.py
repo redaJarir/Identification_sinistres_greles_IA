e83ce2175246f1d946f2e5ac892622330852bfc0
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-11-07 23:09:15 +0100

    modification de quelques packages tensorflow

--- lstm_predictions_function.py
+++ lstm_predictions_function.py
@@ -1,14 +1,14 @@
 #libraries
-import keras
+import tensorflow
 import os
 import numpy as np
+import keras
 #fonction de prédiction avec le modèle lstm
 def predict_lstm(df):
-  my_model = keras.models.load_model('My_model.h5')
+  my_model= tensorflow.keras.models.load_model('My_model.h5')
   x = preparation(df)
   predictions=my_model.predict(x)
   pred = np.array(list(map(lambda x : 'grele' if x > 0.5 else 'non grele',predictions)))
   pred.tolist()
   df['lstm_class'] = pred
   return(df)
-
