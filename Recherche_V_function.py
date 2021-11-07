c3d0a0a26955ce9c4538838557feb8a677dbfeb5
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-11-07 21:52:16 +0100

    supprimer le code qui enlève les chiffres cr il est intégré dans la fonction clean_comment

--- Recherche_V_function.py
+++ Recherche_V_function.py
@@ -1,17 +1,15 @@
 #fonction de Recherche V
 def Recherche_V(df):
-  df['LI_DSC_ADS'] = df['LI_DSC_ADS'].astype(str)
-  df['LI_DSC_SIGMA'] = df['LI_DSC_SIGMA'].astype(str)
-  df['LI_CAUSE'] = df['LI_CAUSE'].astype(str)
-  df['Comment_RV'] = df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS + ' '+df.LI_CAUSE)
-  df['Comment_RV'] = df['Comment_RV'].apply(clean_comment)
-  df['Comment_RV'] = df['Comment_RV'].str.replace('\d+', '')
-  matches=['grel']
-  for i in range (len(df)):
-    df.loc[i, 'Comment_RV']=stem_comment(df.loc[i, 'Comment_RV'])
-    if any(a in df.loc[i, 'Comment_RV'] for a in matches):
-        df.loc[i, 'Recherche_V_class']='grele'
-    else:
-        df.loc[i, 'Recherche_V_class']='non grele'
-  return(df)
-
+    df['LI_DSC_ADS']=df['LI_DSC_ADS'].astype(str)
+    df['LI_DSC_SIGMA']=df['LI_DSC_SIGMA'].astype(str)
+    df['LI_CAUSE']=df['LI_CAUSE'].astype(str)
+    df['Comment_RV']=df.LI_DSC_SIGMA.str.cat(' '+df.LI_DSC_ADS+' '+df.LI_CAUSE)
+    df['Comment_RV']=df['Comment_RV'].apply(clean_comment)
+    match=['grel']
+    for i in range (len(df)):
+      df.loc[i,'Comment_RV']=stem_comment(df.loc[i,'Comment_RV'])
+      if any(a in df.loc[i,'Comment_RV'] for a in match):
+        df.loc[i,'Recherche_V_class']='grele'
+      else:
+        df.loc[i,'Recherche_V_class']='non grele'
+    return(df)
