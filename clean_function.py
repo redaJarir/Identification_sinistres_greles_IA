4bc8bdf8cc92a80a1566c24e4858601c6026ac01
Fatima-Zahra ZEIKAR <zeikarfatimazahra@gmail.com>
2021-11-07 21:50:59 +0100

    Ajout du symbole '+' et Ajout d'un code pour enlever les chiffres

--- clean_function.py
+++ clean_function.py
@@ -5,7 +5,7 @@ nltk.download('punkt')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 #les caractères à enlever 
-REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,-;_:!?]')
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,-;+_:!?]')
 #Importer la liste des stopwords en français 
 STOPWORDS = nltk.corpus.stopwords.words('french')
 #Liste des mots à ajouter dans la liste des stopwords en français
@@ -19,6 +19,7 @@ def clean_comment(comment):
   comment = comment.lower()
   comment = REPLACE_BY_SPACE_RE.sub(' ', comment) # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space. 
   comment = ' '.join(word for word in comment.split() if word not in STOPWORDS) # remove stopwors from text
+  comment = ''.join((word for word in comment if not word.isdigit()))
   ecritures_bizarres=['gr\x88le','grele','gêle', 'gr�le', 'grêle', 'gr le','græle','grële','g^le','gr}le','grle','gr^le']
   for a in ecritures_bizarres:
     if a in comment:
