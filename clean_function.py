5f84a340591f9cb40417176c64842bb5c7d534b1
reda jarir <reda.jarir@gmail.com>
2021-11-12 00:15:58 +0100

    simplification et suppression de l'aspect cumulatif

--- clean_function.py
+++ clean_function.py
@@ -1,19 +1,27 @@
 #Libraries
 import re
 import nltk
+
 nltk.download('punkt')
 nltk.download('stopwords')
+
 from nltk.corpus import stopwords
+
 #les caractères à enlever 
 REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,-;+_:!?]')
+
 #Importer la liste des stopwords en français 
 STOPWORDS = nltk.corpus.stopwords.words('french')
+
 #Liste des mots à ajouter dans la liste des stopwords en français
 words_to_add=['fiche', 'db', 'dpt', 'neant', 'nan', 'na']
+
 for word in words_to_add:
   STOPWORDS.append(word)
+  
 #mots à enlever de la liste des stopwords en français
 STOPWORDS.remove('pas')
+
 #fonction pour faire le cleaning
 def clean_comment(comment):
   comment = comment.lower()
